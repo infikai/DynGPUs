5781fb71f9367dd4ad6348be9a53cd8b4833f5da
Kevin <47602662+infikai@users.noreply.github.com>
2025-06-03 14:12:46 +0800

    Update training parameter

--- testTrain.py
+++ testTrain.py
@@ -11,9 +11,9 @@ import time # For timing epochs
 DATA_DIR = '/mydata/Data/imagenet' # IMPORTANT: Set this to your ImageNet directory
 MODEL_NAME = 'resnet50'
 NUM_CLASSES = 1000 # ImageNet has 1000 classes
-BATCH_SIZE = 64  # Adjust based on your GPU memory
-NUM_WORKERS = 4  # Adjust based on your CPU cores
-LEARNING_RATE = 0.001 # Initial learning rate
+BATCH_SIZE = 256  # Adjust based on your GPU memory
+NUM_WORKERS = 64  # Adjust based on your CPU cores
+LEARNING_RATE = 0.01 # Initial learning rate
 MOMENTUM = 0.9
 WEIGHT_DECAY = 1e-4
 EPOCHS = 90 # Standard for ImageNet from scratch, adjust as needed
