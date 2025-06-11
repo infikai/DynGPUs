c0c695910791d10094e39427402270e680318be3
Kevin <gogamekevin123@gmail.com>
2025-06-11 16:40:35 +0800

    Update pauseandresume.py

--- pauseandresume.py
+++ pauseandresume.py
@@ -161,7 +161,7 @@ def train(st):
 
     print(f"[TRAIN PID {os.getpid()}] Successfully loaded {len(train_dataset)} images from ImageNet training set.")
 
-    num_dataloader_workers = 32 if device.type == 'cuda' else 0
+    num_dataloader_workers = 1 if device.type == 'cuda' else 0
     batch_size = 96 if device.type == 'cuda' else 32
     print(f"[TRAIN PID {os.getpid()}] DataLoader using num_workers={num_dataloader_workers}, batch_size={batch_size}")
     start_time_data_loader = time.time()
