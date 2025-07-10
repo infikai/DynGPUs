cbfdec66694b97cddade24d6619273a5a1bcfb49
Kevin <gogamekevin123@gmail.com>
2025-07-10 15:30:21 +0800

    Update starting.sh

--- horovod/starting.sh
+++ horovod/starting.sh
@@ -5,8 +5,9 @@ nvidia-smi -q -d UTILIZATION,MEMORY -lms 10 -f gpu_usage.txt &
 NVIDIA_SMI_PID=$!
 echo "nvidia-smi started in the background with PID: $NVIDIA_SMI_PID"
 
-echo "Launching training on 1 GPU in the background..."
+echo "Launching training in the background..."
 horovodrun -np 3 --min-np 2 --max-np 3 --network-interface eth0 --host-discovery-script ./discover_hosts.sh --verbose python train_elastic_measurement.py --model resnet50 --batch-size 128 > r50b128 2>&1 &
+sleep 5
 TRAINING_PID=$(pgrep -f "train_elastic_measurement.py" | head -n 1)
 echo "PID: $TRAINING_PID"
 sleep 70
