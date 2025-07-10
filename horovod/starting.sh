ab3a276be20b6692ea7ea895399cb4fda60d374f
Kevin <gogamekevin123@gmail.com>
2025-07-10 11:40:28 +0800

    Create starting.sh

---
+++ horovod/starting.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# --- Start background command ---
+nvidia-smi -q -d UTILIZATION,MEMORY -lms 10 -f gpu_usage.txt &
+NVIDIA_SMI_PID=$!
+echo "nvidia-smi started in the background with PID: $NVIDIA_SMI_PID"
+
+echo "Launching training on 1 GPU in the background..."
+horovodrun -np 3 --min-np 2 --max-np 3 --network-interface eth0 --host-discovery-script ./discover_hosts.sh --verbose python train_elastic_measurement.py --model resnet50 --batch-size 128 > r50b128 2>&1 &
+
+echo "Killing nvidia-smi process (PID: $NVIDIA_SMI_PID)..."
+kill $NVIDIA_SMI_PID
+echo "nvidia-smi process killed."
