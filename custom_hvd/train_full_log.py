bef92d87557df5ac4db68bdb1782cbb5406cdfc2
Kevin <47602662+infikai@users.noreply.github.com>
2025-10-28 11:14:59 -0400

    Update train_full_log.py

--- custom_hvd/train_full_log.py
+++ custom_hvd/train_full_log.py
@@ -210,10 +210,8 @@ def main():
                 except StopIteration:
                     epoch_finished = 1
             if hvd.rank() not in current_active_ranks:
-                hvd_optimizer._unregister_hooks()
-                del hvd_optimizer
-                hvd_optimizer = None
-                base_optimizer.zero_grad()
+                hvd_optimizer.zero_grad()
+                # base_optimizer.zero_grad()
                 move_optimizer_state(base_optimizer, 'cpu')
                 model.cpu()
                 torch.cuda.empty_cache()
