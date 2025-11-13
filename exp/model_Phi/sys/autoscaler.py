5231308159199efe79842bd334710df3a38eeac4
Kevin <47602662+infikai@users.noreply.github.com>
2025-11-13 09:10:35 -0500

    Update autoscaler.py

--- exp/model_Phi/sys/autoscaler.py
+++ exp/model_Phi/sys/autoscaler.py
@@ -14,8 +14,8 @@ SERVER_COUNT_LOG_FILE = "./active_servers.log"
 ACTIVE_WORKERS_FILE = "/mydata/Data/DynGPUs/exp/model_Phi/custom_hvd/active_workers.txt"
 
 # Scaling Thresholds (based on average (running + waiting) requests per server)
-SCALE_DOWN_THRESHOLD = 8
-SCALE_UP_THRESHOLD = 12
+SCALE_DOWN_THRESHOLD = 10
+SCALE_UP_THRESHOLD = 15
 
 # Scaling Rules
 MIN_ACTIVE_SERVERS = 1
