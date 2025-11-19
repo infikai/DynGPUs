aeced6d70f8815faf461b630f81f5967838b3315
Kevin <47602662+infikai@users.noreply.github.com>
2025-11-19 07:25:58 -0500

    para

--- sim_delay/proposed/run.py
+++ sim_delay/proposed/run.py
@@ -8,8 +8,8 @@ from components import Job
 import time
 
 # --- NEW: Define Pool Sizes ---
-TRAINING_POOL_SIZE = 600
-INFERENCE_POOL_SIZE = 500
+TRAINING_POOL_SIZE = 550
+INFERENCE_POOL_SIZE = 450
 
 def load_jobs_from_csv(file_path):
     """
