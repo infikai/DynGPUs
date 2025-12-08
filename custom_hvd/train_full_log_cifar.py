01247d8fb9552a2201bd225441031380bb1c2573
Kevin <47602662+infikai@users.noreply.github.com>
2025-12-08 12:04:48 -0500

    Update train_full_log_cifar.py

--- custom_hvd/train_full_log_cifar.py
+++ custom_hvd/train_full_log_cifar.py
@@ -68,7 +68,7 @@ def main():
     train_dataset = datasets.CIFAR10(root='./data', 
                                         train=True, 
                                         download=True,
-                                        transform=transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]))
+                                        transform=transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]))
     sampler = MyElasticSampler(train_dataset)
 
     state = TrainingState()
