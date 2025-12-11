30387b0b84170a98ef8447b22266051b9f958905
Kevin <47602662+infikai@users.noreply.github.com>
2025-12-10 20:08:29 -0500

    Update benchmark_api_trace.py

--- sys_test_LB/benchmark_api_trace.py
+++ sys_test_LB/benchmark_api_trace.py
@@ -12,13 +12,13 @@ import numpy as np
 
 
 # --- Global Configuration ---
-STAGE_DURATION_SECONDS = 300  # 5 minutes per stage
+STAGE_DURATION_SECONDS = 500  # 5 minutes per stage
 BENCHMARK_STAGES = {
     "Stage 1 (Low Load)": 0.75,
     "Stage 2 (Low Load)": 0.35,
     "Stage 3 (Low Load)": 1,
-    "Stage 4 (Low Load)": 0.7,
-    "Stage 5 (Low Load)": 0.35,
+    "Stage 4 (Low Load)": 0.35,
+    "Stage 5 (Low Load)": 0.70,
 }
 REQUEST_READ_TIMEOUT_SECONDS = 600
 
