e6afb69e9eba7c65cdaca33cb1bab9476c80691a
Kevin <47602662+infikai@users.noreply.github.com>
2026-01-09 14:45:15 -0800

    Update autoscaler2.py

--- sys_v100_controller/autoscaler2.py
+++ sys_v100_controller/autoscaler2.py
@@ -15,8 +15,8 @@ ACTIVE_WORKERS_FILE = "/home/pacs/Kevin/DynGPUs/sys_v100/active_workers.txt"
 TTFT_LOG_FILE = "./ttft_controller.log"
 
 # Base Scaling Thresholds
-SCALE_DOWN_THRESHOLD = 11
-SCALE_UP_THRESHOLD = 21
+SCALE_DOWN_THRESHOLD = 15
+SCALE_UP_THRESHOLD = 25
 
 # Scaling Rules
 MIN_ACTIVE_SERVERS = 1
