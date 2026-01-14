a7b13f57de21cd84ca20016e2c2a7bcccc7104c3
Kevin <47602662+infikai@users.noreply.github.com>
2026-01-14 09:58:49 -0500

    Update autoscaler.py

--- testbed/model_llama/Proposed/autoscaler.py
+++ testbed/model_llama/Proposed/autoscaler.py
@@ -15,8 +15,8 @@ ACTIVE_WORKERS_FILE = "./active_workers.txt"
 TTFT_LOG_FILE = "./ttft_controller.log"
 
 # Base Scaling Thresholds
-SCALE_DOWN_THRESHOLD = 13
-SCALE_UP_THRESHOLD = 23
+SCALE_DOWN_THRESHOLD = 30
+SCALE_UP_THRESHOLD = 40
 
 # Scaling Rules
 MIN_ACTIVE_SERVERS = 1
