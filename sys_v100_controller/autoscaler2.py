8cceddb3731796a24dc16f1e3ff98ac78680c393
Kevin <47602662+infikai@users.noreply.github.com>
2026-01-23 21:00:17 -0500

    Update autoscaler2.py

--- sys_v100_controller/autoscaler2.py
+++ sys_v100_controller/autoscaler2.py
@@ -15,8 +15,8 @@ ACTIVE_WORKERS_FILE = "./active_workers.txt"
 TTFT_LOG_FILE = "./ttft_controller.log"
 
 # Base Scaling Thresholds
-SCALE_DOWN_THRESHOLD = 32
-SCALE_UP_THRESHOLD = 50
+SCALE_DOWN_THRESHOLD = 23
+SCALE_UP_THRESHOLD = 39
 
 # Scaling Rules
 MIN_ACTIVE_SERVERS = 1
