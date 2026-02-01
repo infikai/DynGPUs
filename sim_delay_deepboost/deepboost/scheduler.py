87f21ee6dbbf428d8e6e4bee5afed203ac5acdfe
Kevin <47602662+infikai@users.noreply.github.com>
2026-02-01 16:59:57 -0500

    Update scheduler.py

--- sim_delay_deepboost/deepboost/scheduler.py
+++ sim_delay_deepboost/deepboost/scheduler.py
@@ -483,8 +483,8 @@ class Scheduler:
                 gpu.update_lifecycle(self.clock.tick_duration)
 
             if self.clock.current_time >= next_scaling_check:
-                # self._try_scale_up_training_jobs()
-                next_scaling_check = self.clock.current_time + 60
+                self._try_scale_up_training_jobs()
+                next_scaling_check = self.clock.current_time + 5000
 
             self._process_scaling_events()
 
